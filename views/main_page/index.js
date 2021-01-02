eda9071cc0413105213020215d81744346af4954
unknown <vanamokanov8@gmail.com>
2021-01-03 02:56:38 +0700

    I've started to make new view

--- views/main_page/index.js
+++ views/main_page/index.js
@@ -1,10 +1,5 @@
 const musicTags = require('../../utils/musicTags')
 const remote = require('electron').remote
-console.log(require('electron'))
-console.log(require('electron').remote.getGlobal('state'))
-const state = require('electron').remote.getGlobal('state')
-
-
 
 //Hidden audio tag
 const hear = document.getElementById('listen');
@@ -12,15 +7,22 @@ const hear = document.getElementById('listen');
 const progressBar = document.querySelector('.music-progress-bar')
 //Buttons under progress bar
 const pauseButton = document.querySelector('.pause-button')
-//Input field with name of song, can be changed
+const previousButton = document.querySelector('.previous-button')
+const nextButton = document.querySelector('.next-button')
+//Input field with name of song, they can be changed
 const songName = document.querySelector('.song-name')
 const songAuthor = document.querySelector('.song-author')
-const songPath = state.songName
+
+
+//Initial state in the load of main page view
+let state = remote.getGlobal('state')
+
 
 //Set song to play
-async function setSong(song){
-    hear.src = songPath
-    setSongData(songPath)
+async function setSong(){
+    const song = state.songName
+    hear.src = song
+    setSongData(song)
     if(state.isSongPlaying){
         playMusic()
     }else{
@@ -28,7 +30,7 @@ async function setSong(song){
     }
 }
 
-setSong(songPath)
+setSong()
 
 //Set different data like name(name of music file) or different ID3 tags
 async function setSongData(songPath)
@@ -60,7 +62,7 @@ function setMusicProgress(){
     progressBar.value = progress;
 }
 
-//Set the current time oof music, depends on the point on the progress bar
+//Set the current time of music, depends on the point on the progress bar
 function setMusicMoment(){
     const currentTime = (hear.duration * progressBar.value) / 1000
     console.log(`
@@ -69,6 +71,35 @@ function setMusicMoment(){
         Duration: ${hear.duration}
     `)
     hear.currentTime = currentTime
+}
+
+
+//Set new song, for that function use another function - setSong
+//Replay the playlist in case the song was last and set the next song in the other way
+function setNextSong(){
+    const newState = remote.getGlobal('state')
+    const indexOfCurrentSong = newState.playList.indexOf(newState.songName)
+    if(indexOfCurrentSong >= newState.playList.length){
+        newState.songName = newState.playList[0]
+    }else{
+        newState.songName = newState.playList[indexOfCurrentSong + 1]
+    }
+
+    setSong()
+}
+
+//Set new song, for that function use another function - setSong
+//Play the last song in playlist if current song was first or play previous song in playlist
+function setPreviousSong(){
+    const newState = remote.getGlobal('state')
+    const indexOfCurrentSong = newState.playList.indexOf(newState.songName)
+    if(indexOfCurrentSong <= 0){
+        newState.songName = newState.playList[newState.playList.length - 1]
+    }else{
+        newState.songName = newState.playList[indexOfCurrentSong - 1]
+    }
+
+    setSong()
 }
 
 //If music is playing now, pause button stops it, in the other way music starts playing
@@ -81,17 +112,27 @@ pauseButton.onclick = () => {
     }
 }
 
+//Set previous song when user press previous button
+previousButton.onclick = () => {
+    setPreviousSong()
+}
+
+//Set next song when user press next button
+nextButton.onclick = () => {
+    setNextSong()
+}
+
+
 //Pause music when music pauses in audio element.
 //First of all it's for case when user press pause button on keyborad or kind of.
-hear.addEventListener('pause', (e) => {
-    pauseMusic()
-})
+hear.addEventListener('pause', pauseMusic)
 
 //Starts to play music when music starts to play in audio element.
 //First of all it's for case when user press play button on keyborad or kind of.
-hear.addEventListener('play', ()=>{
-    playMusic()
-})
+hear.addEventListener('play', playMusic)
+
+//When music ends, programms automaticly set next song
+hear.addEventListener('ended', setNextSong)
 
 //When user set music time moment on music progress bar, we have to update music current time in audio tag
 progressBar.onchange = () => {
@@ -102,10 +143,12 @@ progressBar.onchange = () => {
 setInterval(setMusicProgress, 500)
 
 
+
 songName.addEventListener('change', () => {
     const tags = {
         'NAME': songName.value
     }
+    const songPath = state.songName
     musicTags.writeSongData(songPath, tags)
 })
 
@@ -113,5 +156,6 @@ songAuthor.addEventListener('change', ()=>{
     const tags = {
         'TPE1': [songAuthor.value]
     }
+    const songPath = state.songName
     musicTags.writeSongData(songPath, tags)
 })
