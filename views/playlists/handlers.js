d2159dd3e3f25b849768ad31ac3c5c7af92027c9
unknown <vanamokanov8@gmail.com>
2021-01-03 18:04:35 +0700

    I've completed design of playlists view

---
+++ views/playlists/handlers.js
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------------------------
+// It is the file, which contains different listeners and functions onClick, etc.
+//-------------------------------------------------------------------------------
+
+const dropdownButton = document.querySelector('.change-playlist-button')
+const currentPlaylistName = document.querySelector('.current-playlist-name')
+const dropdownMenu = document.querySelector('.dropdown')
+
+//Show dropdown if it ain't visible now and in the other way hides it
+function changeDropdownVisibility(){
+    const isdropdownVisible = dropdownMenu.style.display == "block"
+    if(isdropdownVisible){
+        dropdownMenu.style.display = "none"
+    }else{
+        dropdownMenu.style.display = "block"
+    }
+}
+
+dropdownButton.onclick = changeDropdownVisibility
+currentPlaylistName.onclick = changeDropdownVisibility
+
+setInterval(doStuff, 200000)
